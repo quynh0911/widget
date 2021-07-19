a74481c69d053eab5fcee327ce61323df54371f0
quynh0911 <phamthihuongquynh.toan@gmail.com>
2021-07-19 11:19:55 +0700

    col

--- lib/ui/col.dart
+++ lib/ui/col.dart
@@ -4,6 +4,7 @@ class MyCol extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         Text("data1"),
         Text("data2"),
