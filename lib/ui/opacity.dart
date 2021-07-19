e5a3264b5df3a9a3dbe11568471be9112cc2501e
quynh0911 <phamthihuongquynh.toan@gmail.com>
2021-07-19 09:40:06 +0700

    change opacity

--- lib/ui/opacity.dart
+++ lib/ui/opacity.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MyOpacity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Opacity(
-      opacity: 0.5,
-      child: const Text("Hi!"),
+    return Stack(
+      children: [
+        Opacity(
+          opacity: 0.2,
+          child: Container(
+            color: Colors.black,
+          ),
+        ),
+        Center(
+          child: Text(
+            "Opacity: 0.2",
+            style: TextStyle(
+                backgroundColor: Colors.white,
+                color: Colors.red,
+                fontWeight: FontWeight.w700),
+          ),
+        )
+      ],
     );
   }
 }
